5395cb13ea0f07a33840b123e3dbdc5f0bea9491
Rushiraj Wani <rajwani2003@gmail.com>
2026-04-08 16:20:03 +0530

    Add server setup with Express, CORS, and dotenv configuration

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+const authRoutes = require("./routes/authRoutes");
+// const aiRoutes = require("./routes/aiRoutes");
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+const connectDB = require("./config/db");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/ai", aiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
